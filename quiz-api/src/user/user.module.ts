c06aa4e4b94d9be5e1a8083cb6167f2643b43b3d
Ruslan Gonzalez <ruslanguns@gmail.com>
2020-11-15 21:05:22 +0000

    quiz-api: added user service with its api

--- quiz-api/src/user/user.module.ts
+++ quiz-api/src/user/user.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities';
+import { UserService } from './user.service';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User])
-  ]
+  ],
+  providers: [UserService]
 })
 export class UserModule {}
---
+++ quiz-api/src/user/user.service.ts
@@ -0,0 +1,39 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities';
+
+@Injectable()
+export class UserService {
+
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>
+  ) {}
+
+  async getMany(): Promise<User[]> {
+    return await this.userRepository.find();
+  }
+
+  async getByUsername(username: string): Promise<User> {
+    return await this.userRepository
+      .createQueryBuilder('user')
+      .where({ username })
+      .addSelect('user.password')
+      .getOne();
+  }
+
+  async getById(id: number): Promise<User> {
+    const user = await this.userRepository.findOne(id);
+    if (!user) {
+      throw new NotFoundException(`USER DOES NOT EXIST`);
+    }
+    return user;
+  }
+
+  async deleteOne(id: number): Promise<User> {
+    const user = await this.getById(id);
+    return await this.userRepository.remove(user);
+  }
+
+}
