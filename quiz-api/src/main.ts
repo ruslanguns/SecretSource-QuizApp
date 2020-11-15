421f2cf8e42f653e5f2983f6e27887311020bb0e
Ruslan Gonzalez <ruslanguns@gmail.com>
2020-11-15 18:02:23 +0000

    quiz-api: added @nestjs/config module for env

--- quiz-api/src/main.ts
+++ quiz-api/src/main.ts
@@ -1,8 +1,11 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new Logger('QuizAPI');
+  await app.listen(process.env.SERVER_PORT || 3000);
+  logger.verbose(`Server is running on ${await app.getUrl()}`)
 }
 bootstrap();
