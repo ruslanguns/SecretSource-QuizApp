62a2e7cf9dafc6e38ecbdea5eeeaac4b6f073a35
Ruslan Gonzalez <ruslanguns@gmail.com>
2020-11-20 15:18:08 +0000

    quiz-app: @angular upgraded to 11.0.2

--- quiz-app/e2e/src/app.e2e-spec.ts
+++ quiz-app/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('quizApp app is running!');
+    expect(await page.getTitleText()).toEqual('quiz-app app is running!');
   });
 
   afterEach(async () => {
