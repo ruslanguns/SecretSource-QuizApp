2c53c39988bab79b664b2e4bbe93404a72037a99
Ruslan Gonzalez <ruslanguns@gmail.com>
2020-11-26 00:43:18 +0000

    quiz-app: redirect logged users from auth pages to home

--- quiz-app/src/app/auth/auth-routing.module.ts
+++ quiz-app/src/app/auth/auth-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginGuard } from '../core/guards/login.guard';
 import { AuthComponent } from './auth.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
@@ -8,6 +9,7 @@ const routes: Routes = [
   {
     path: '',
     component: AuthComponent,
+    canActivateChild: [LoginGuard],
     children: [
       { path: 'login', component: LoginComponent },
       { path: 'register', component: RegisterComponent },
